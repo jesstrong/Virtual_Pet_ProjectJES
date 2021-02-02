30adbf51a31b46be3fb5693babc8c24bc6590435
JES Strong <janetestrong@yahoo.com>
2021-02-02 15:13:19 -0500

    Added User Main menu

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -18,7 +18,64 @@ namespace VirtualPet
 
             userPet.SetName(Console.ReadLine());
 
+            Console.Clear();
+
             Console.WriteLine("Your pet is " + userPet.GetName() + " the " + userPet.GetSpecies());
+
+            bool keepPlaying = true;
+
+            while (keepPlaying) 
+            {
+                Console.WriteLine("What do you want to do with " + userPet.GetName());
+
+                Console.WriteLine("1. Feed");
+
+                Console.WriteLine("2. Play");
+
+                Console.WriteLine("3. See doctor?");
+
+                Console.WriteLine("4. Exit");
+
+                string userChoice = Console.ReadLine();
+
+                switch (userChoice)
+                {
+                    case "1":
+                        Console.WriteLine("You fed " + userPet.GetName());
+                        userPet.Feed();
+                        break;
+                    case "2":
+                        Console.WriteLine("You played with " + userPet.GetName());
+                        userPet.Play();
+                        break;
+                    case "3":
+                        Console.WriteLine(userPet.GetName() + " feels alot better now");
+                        userPet.SeeDoctor();
+                        break;
+                    case "4":
+                        Console.WriteLine("Thank you for playing!");
+                        keepPlaying = false;
+                        break;
+                    default:
+                        Console.WriteLine("You have angered Horace. Please choose a proper option!");
+                        break; 
+                        
+                }
+
+
+
+
+
+                                 
+                
+
+
+
+
+            } 
+
+
+            
         }
     }
 }
