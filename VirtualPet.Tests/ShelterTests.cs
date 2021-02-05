91c3f054efdf8e3f27a20ded542d6936054e4228
GavHensley <gavinrhensley@gmail.com>
2021-02-05 11:55:25 -0500

    Attempted a test and failed in ShelterTests

---
+++ VirtualPet.Tests/ShelterTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace VirtualPet.Tests
+{
+    public class ShelterTests
+    {
+        Shelter testShelter;
+
+        public ShelterTests()
+        {
+            testShelter = new Shelter();
+        }
+
+        [Fact] 
+        public void AddPet_Should_Increase_List_Count_by_1()
+        {
+            //Arrange
+            testShelter.ListOfPets.Clear();
+            Pet newPet = new Pet();
+            //Act
+            testShelter.AddPet(newPet);
+            //Assert
+            Assert.Single(testShelter.ListOfPets);
+        }
+        [Fact]
+        public void SelectPet_Should_Allow_User_To_Pick_Pet_From_Shelter()
+        {
+            //Arrange
+            testShelter.ListOfPets.Clear();
+            Pet pet1 = new Pet();
+            Pet pet2 = new Pet();
+            testShelter.AddPet(pet1);
+            testShelter.AddPet(pet2);
+            //Act
+            //Assert
+            Assert.Equal(pet2, testShelter.ListOfPets[1]);
+
+        }
+    }
+}
--- VirtualPet/Shelter.cs
+++ VirtualPet/Shelter.cs
@@ -4,9 +4,27 @@ using System.Text;
 
 namespace VirtualPet
 {
-    class Shelter
+    public class Shelter
     {
+        public List<Pet> ListOfPets = new List<Pet>();
 
 
+        public void AddPet(Pet pet)
+        {
+            ListOfPets.Add(pet);
+        }
+
+        //public Pet SelectPet()
+        //{
+        //    int index = 1;
+
+        //    Console.WriteLine("Please select a pet from the list.");
+        //    foreach (Pet pet in ListOfPets)
+        //    {
+        //        Console.WriteLine($"{index});
+
+        //    }
+        //}
+        
     }
 }
